8d6fa7ab6f9cd00e1cb58e5b285f164f79e73b30
nrup323 <ms18btech11015@iith.ac.in>
2022-02-23 10:38:49 +0530

    added self everywhere

--- data/fudan_dataset.py
+++ data/fudan_dataset.py
@@ -26,20 +26,21 @@ class FudanDataset(BaseDataset):
         self.img_paths = []
         # scenes = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 36, 37, 38, 39, 40, 46, 47, 48, 49, 50, 61, 62, 63, 64, 65,
         #           66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 91, 92, 93, 94, 95]
-        scenes = [1,11,12,13,16,17,18,2,21,22,23,26,27,28,3,31,32,33,36,37,38,41,42,43,46,47,48,
+        scenes = [1, 11,12,13,16,17,18,2,21,22,23,26,27,28,3,31,32,33,36,37,38,41,42,43,46,47,48,
         51,52,53,56,57,58,6,61,62,63,66,67,68,7,71,72,73,76,77,78,8,81,82,83,86,87,88,91,92,93,96,97,98,]
         scene_dirs = [os.path.join(self.root, str(scene)) for scene in scenes]
         #scene_dirs = [os.path.join(self.root, scene) for scene in os.listdir(self.root)]
         for scene_dir in scene_dirs:
-            den_paths = sorted(make_dmap_dataset(scene_dir))
-            img_paths = sorted(make_png_dataset(scene_dir))
-            assert(len(den_paths) == len(img_paths))
+            self.den_paths = sorted(make_dmap_dataset(scene_dir))
+            self.img_paths = sorted(make_png_dataset(scene_dir))
+            print(f"Length of the img_paths is  : {len(self.img_paths)}")
+            assert(len(self.den_paths) == len(self.img_paths))
             for t_step in range(self.min_t_step, self.max_t_step+1):
                 offset = (self.tG - 1) * t_step
-                for i in range(len(img_paths) - offset):
-                    imgs = [img_paths[i+j*t_step] for j in range(self.tG)]
-                    self.img_paths.append(imgs)
-                    dens = [den_paths[i+j*t_step] for j in range(self.tG)]
+                for i in range(len(self.img_paths) - offset):
+                    imgs = [self.img_paths[i+j*t_step] for j in range(self.tG)]
+                    self.self.img_paths.append(imgs)
+                    dens = [self.den_paths[i+j*t_step] for j in range(self.tG)]
                     self.den_paths.append(dens)
         assert(len(self.img_paths) == len(self.den_paths))
 
@@ -135,17 +136,17 @@ class TestFetcher():
     def __next__(self):
         if self.index == len(self.dmap_paths) - len(self.img_paths):
             raise StopIteration()
-        img_paths = [os.path.join(self.results_dir, path) for path in os.listdir(self.results_dir)]
-        img_paths.sort()
-        img_paths = img_paths[self.index:self.index+self.n_frames_G-1]
-        print(img_paths)
+        self.img_path = [os.path.join(self.results_dir, path) for path in os.listdir(self.results_dir)]
+        self.img_path.sort()
+        self.img_path = self.img_path[self.index:self.index+self.n_frames_G-1]
+        print(self.img_path)
         dmap_paths = self.dmap_paths[self.index: self.index+self.n_frames_G]
         imgs = dmaps = 0
-        for i,img_path in enumerate(img_paths):
+        for i,img_path in enumerate(self.img_path):
             img = Image.open(img_path)
             img = self.img_transform(img)
             imgs = img if i == 0 else torch.cat([imgs, img], dim=0)
-            if i == len(img_paths) - 1:
+            if i == len(self.img_path) - 1:
                 imgs = torch.cat([imgs, img], dim=0)
 
         for i,dmap_path in enumerate(dmap_paths):
@@ -155,8 +156,9 @@ class TestFetcher():
         imgs = imgs.unsqueeze(0)
         dmaps = dmaps.unsqueeze(0)
         self.index += 1
-        return {'D': dmaps, 'I': imgs, 'D_path': dmap_paths[-1], 'I_path': img_paths[-1]}
+        return {'D': dmaps, 'I': imgs, 'D_path': dmap_paths[-1], 'I_path': self.img_path[-1]}
 
     def __len__(self):
         return len(self.dmap_paths) - len(self.img_paths)
+
 
